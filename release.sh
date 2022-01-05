76ca5027ce1272e333689a233d2d443dd0224ab4
Harish Bohara <harish.bohara@gmail.com>
2022-01-05 17:23:31 +0530

    added sample project

--- release.sh
+++ release.sh
@@ -1,1 +1,7 @@
 mvn -B clean install -Dtag=2.0.3 -Dmaven.test.skip=true release:prepare release:perform -P release
+
+
+# Rollback
+# mvn release:rollback
+# git tag -d 2.0.2
+# git push --delete origin 2.0.2
